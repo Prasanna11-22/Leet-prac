10dad498a3799b70bea63970ae71c8068862a6d2
PRASANNA S <psp444333@gmail.com>
2026-07-20 15:38:42 +0530

    Time: 10 ms (70.68%), Space: 35.1 MB (54.11%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+
+bool isposs(vector<int>& weights,int w,int days)
+{
+    int sum=0;
+    int dayused=1;
+
+    for(int i : weights)
+    {
+        if(i+sum<=w) sum+=i;
+        else
+        {
+            sum=i;
+            dayused++;
+        }
+    }
+    return dayused<=days;
+
+}
+    int shipWithinDays(vector<int>& weights, int days) {
+       int l=*max_element(weights.begin(),weights.end());
+       int r=accumulate(weights.begin(),weights.end(),0);
+       int ans=0;
+       while(l<=r)
+       {
+        int mid=l+(r-l)/2;
+        bool t=isposs(weights,mid,days);
+        cout<<t<<endl;
+        if(t)
+        {
+            ans=mid;
+            r=mid-1;
+        }
+        else
+        {
+            l=mid+1;
+        }
+       } 
+       return ans;
+    }
+};
