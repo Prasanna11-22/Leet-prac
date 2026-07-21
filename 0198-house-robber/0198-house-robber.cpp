f20035b9094e5dfcf6dcab9138b8fd09b5761834
PRASANNA S <psp444333@gmail.com>
2026-07-21 11:30:00 +0530

    Time: 0 ms (100%), Space: 10.8 MB (23.79%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    int rob(vector<int>& arr) {
+        int n=arr.size();
+        vector<int> dp(n,0);
+        
+        if(n==1) return arr[0];
+        if(n==2) return max(arr[0],arr[1]);
+        dp[0]=arr[0];
+        dp[1]=max(arr[0],arr[1]);
+        
+        for(int i=2;i<n;i++)
+        {
+            dp[i]=max(dp[i-1],(arr[i]+dp[i-2]));
+        }
+        
+        return dp[n-1];
+    }
+};
