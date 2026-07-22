52ee22e567383da8a1995155c61a83e8dfa34c61
PRASANNA S <psp444333@gmail.com>
2026-07-22 14:27:06 +0530

    Time: 31 ms (30.93%), Space: 34.6 MB (14.75%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int n=s.size(),cnt=0;
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        
-        for(int k=0;k<n;k++) //difference
-        {
-            for(int i=0,j=i+k;i<n&&j<n;i++,j++)
-            {
-                if(i==j)
-                {
+        int n=s.length();
+        int count=0;
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int diff=0;diff<n;diff++){
+            for(int i=0,j=i+diff;j<n && i<n ;i++,j++){
+                 if(i==j){
                     dp[i][j]=1;
-                }
-                else if(j-i==1)
-                {   if(s[i]==s[j])
-                    dp[i][j]=2;
-                    else 
-                    dp[i][j]=0;
-                }
-                else
-                {   if(s[i]==s[j] && dp[i+1][j-1]!=0)
-                    dp[i][j]=2+dp[i+1][j-1];
-                }
-
-                if(dp[i][j]!=0)
-                {
-                    cnt++;
-                }
+                 }
+                 else if(diff==1){
+                    if(s[i]==s[j]){
+                        dp[i][j]=2;
+                    }
+                 }
+                 else{
+                    if(s[i]==s[j]&& dp[i+1][j-1]!=0){
+                        dp[i][j]=2+dp[i+1][j-1];
+                    }
+                 }
+                 if(dp[i][j]!=0){
+                    count++;
+                 }
             }
         }
-        
-        return cnt;
+        return count;
     }
 };
