5d5b0e133646d238a084dd7efd4f9cd7ff6dc804
PRASANNA S <psp444333@gmail.com>
2026-07-22 09:55:58 +0530

    Time: 7 ms (60.83%), Space: 13.3 MB (30.78%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -4,20 +4,28 @@ public:
         int s1=w1.size(),s2=w2.size();
         vector<vector<int>> dp(s1+1,vector<int>(s2+1,0));
 
+        // for(int i=0;i<=s1;i++)
+        // {
+        //     dp[i][0]=i;
+        // }
+        // for(int j=0;j<=s2;j++)
+        // {
+        //     dp[0][j]=j;
+        // }
+
         for(int i=0;i<=s1;i++)
         {
-            dp[i][0]=i;
-        }
-        for(int j=0;j<=s2;j++)
-        {
-            dp[0][j]=j;
-        }
-
-        for(int i=1;i<=s1;i++)
-        {
-            for(int j=1;j<=s2;j++)
+            for(int j=0;j<=s2;j++)
             {
-                if(w1[i-1]==w2[j-1])
+                if(i==0)
+                {
+                    dp[i][j]=j;
+                }
+                else if(j==0)
+                {
+                    dp[i][j]=i;
+                }
+                else if(w1[i-1]==w2[j-1])
                 {
                     dp[i][j]=dp[i-1][j-1];
                 }
