3353841f087b1a1ef349ff35b825720f02c5e04c
PRASANNA S <psp444333@gmail.com>
2026-07-24 14:42:25 +0530

    Time: 14 ms (35.57%), Space: 35.2 MB (19.73%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,49 +1,38 @@
 class Solution {
 public:
-
-
-bool isposs(vector<int>& weights,int w,int days)
+bool isposs(int w,vector<int>& weights,int days)
 {
-    int sum=0;
-    int dayused=1;
-
-    for(int i : weights)
+    int sum=0,daysused=1;
+    for(int x : weights)
     {
-        if(i+sum<=w) sum+=i;
-        else
-        {
-            sum=i;
-            dayused++;
+        sum+=x;
+        if(sum>w){
+            daysused++;
+            sum=x;
         }
+        
     }
-    return dayused<=days;
 
+    return (daysused<=days);
 }
     int shipWithinDays(vector<int>& weights, int days) {
+        int ans=0;
+        int l=*max_element(weights.begin(),weights.end());
+        int r=accumulate(weights.begin(),weights.end(),0);
 
-       
-       int l=*max_element(weights.begin(),weights.end());
-       int r=accumulate(weights.begin(),weights.end(),0);
-       int ans=0;
-
-    //    for(int i=l;i<=r;i++) cout<<i<<"->"<<isposs(weights,i,days)<<endl;
-    //    cout<<endl;
-
-       while(l<=r)
-       {
-        int mid=l+(r-l)/2;
-        bool t=isposs(weights,mid,days);
-       // cout<<mid<<"->"<<t<<endl;
-        if(t)
+        while(l<=r)
         {
-            ans=mid;
-            r=mid-1;
+            int mid=l+(r-l)/2;
+            if(isposs(mid,weights,days))
+            {
+               ans=mid;
+               r=mid-1; 
+            }
+            else
+            {
+                l=mid+1;
+            }
         }
-        else
-        {
-            l=mid+1;
-        }
-       } 
-       return ans;
+        return ans;
     }
 };
