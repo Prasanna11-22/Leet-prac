ef4bb6fbcff4bf59f5d04be7df854bbc717b2703
PRASANNA S <psp444333@gmail.com>
2026-07-24 10:27:54 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50.59%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int mySqrt(int x) {
       if(x<2) return x;
-      int l=1,r=x/2,res=1;
+      int l=0,r=x/2,res=1,i=1;
       while(l<=r)
       {
         long long mid=l+(r-l)/2;
+       //cout<<i++<<" "<<l<<" "<<r<<" "<<mid<<"->"<<mid*mid<<endl;
         if((mid*mid)<=x)
         {
             res=mid;
@@ -17,7 +18,7 @@ public:
         }
       }
     
-    return res;
+    return r;
         
     }
 };
