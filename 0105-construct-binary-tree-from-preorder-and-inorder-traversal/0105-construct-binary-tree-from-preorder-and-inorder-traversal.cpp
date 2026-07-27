fd61b00947dd67788d32771f5574e1fd4571e022
PRASANNA S <psp444333@gmail.com>
2026-07-27 14:08:11 +0530

    Time: 6 ms (36.46%), Space: 27.6 MB (33.84%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,29 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    unordered_map<int, int> inorderIndex;
-    int preorderIndex = 0;
+unordered_map<int,int> mp;
+ int preorderind = 0;
 
-    TreeNode* build(vector<int>& preorder, vector<int>& inorder,int left, int right) {
+TreeNode* rec(vector<int>& preorder, vector<int>& inorder,int l,int r) 
+{    if(l>r) return nullptr;
 
-        if (left > right)
-            return nullptr;
+    int val=preorder[preorderind++];
+    TreeNode* root=new TreeNode(val);
 
-        int rootValue = preorder[preorderIndex++];
-        TreeNode* root = new TreeNode(rootValue);
+    int mid = mp[val];
 
-        int mid = inorderIndex[rootValue];
+    root->left=rec(preorder,inorder,l,mid-1);
+    root->right=rec(preorder,inorder,mid+1,r);
 
-        root->left = build(preorder, inorder, left, mid - 1);
-        root->right = build(preorder, inorder, mid + 1, right);
+    return root;
+}
 
-        return root;
-    }
 
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    for (int i = 0; i < inorder.size(); i++)
+    mp[inorder[i]] = i;
 
-        for (int i = 0; i < inorder.size(); i++)
-            inorderIndex[inorder[i]] = i;
-
-        return build(preorder, inorder, 0, inorder.size() - 1);
+    return rec(preorder,inorder,0,inorder.size()-1);
     }
 };
