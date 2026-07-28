8e2193c0a78ff2f7c4a5089a07316b9539ec0a99
PRASANNA S <psp444333@gmail.com>
2026-07-28 11:14:41 +0530

    Time: 7 ms (95.21%), Space: 15.6 MB (68.3%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+vector<int> res;
+
+void preod(Node* root)
+{
+    if(root==NULL) return;
+    res.push_back(root->val);
+
+    for(auto i : root->children)
+    {
+        preod(i);
+    }
+}
+    vector<int> preorder(Node* root) {
+        preod(root);
+        return res;
+    }
+};
