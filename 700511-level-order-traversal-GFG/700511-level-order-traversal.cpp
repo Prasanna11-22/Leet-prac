9f3bb00125d675f56fec55647c9432e08a0d873b
PRASANNA S <psp444333@gmail.com>
2026-08-04 09:57:53 +0530

    Added solution - Time: O(N), Space: O(N)

---
+++ 700511-level-order-traversal-GFG/700511-level-order-traversal.cpp
@@ -0,0 +1,46 @@
+/* Structure of Binary Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};*/
+
+class Solution {
+  public:
+    vector<int> levelOrder(Node *root) {
+        
+        vector<int> ans;
+       if(root==nullptr) return ans;
+        
+        queue<Node*> q;
+        q.push(root);
+        
+        
+        while(!q.empty())
+        {
+            Node* temp=q.front();
+            q.pop();
+            
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+            
+          ans.push_back(temp->data);
+        }
+        
+        
+        
+        return ans;
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
