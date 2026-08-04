c62e357785eb9c08236dc32d9f1fd1d31cf30fb6
PRASANNA S <psp444333@gmail.com>
2026-08-04 13:50:21 +0530

    0 ms (100%) 109.6 MB (12.06%) 1161-maximum-level-sum-of-a-binary-tree Medium Tree

---
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        int count=1;
+        if(root==nullptr) return 0;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        int maxi=INT_MIN,maxc=-1;
+        while(!q.empty())
+        {
+            int n=q.size();
+            int sum=0;
+            for(int i=0;i<n;i++)
+            {
+                TreeNode* temp=q.front();
+                q.pop();
+                sum+=(temp->val);
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+            if(sum>maxi)
+            {
+                maxi=sum;
+                maxc=count;
+            }
+            count++;
+        }
+        return maxc;
+    }
+};
