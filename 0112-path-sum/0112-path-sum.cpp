31bc74b4f4fe259b05d0645c3f91ba31ef9306cf
PRASANNA S <psp444333@gmail.com>
2026-08-06 10:28:47 +0530

    0 ms (100%) 21.6 MB (36.65%) 0112-path-sum Easy Tree

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-
-int sum=0;
-bool preord(TreeNode* root,int tsum,int sum)
+bool isleaf(TreeNode* root)
 {
-    if(root==NULL) return false;
-
-    int totsum=sum+root->val;
-
-    if(root->left==NULL && root->right==NULL)
-    return (tsum==totsum);
-
- return preord(root->left,tsum,totsum) || preord(root->right,tsum,totsum);
-
+    return (root)&&(!root->left)&&(!root->right);
 }
-    bool hasPathSum(TreeNode* root, int tsum) {
-        return preord(root,tsum,0);
+ bool rec(TreeNode* root,int targ,int sum)
+ {
+    if(!root) return false;
+
+    sum+=root->val;
+
+    if(isleaf(root))
+    {
+        return (sum==targ);
+    }
+
+    return  rec(root->left,targ,sum)|| rec(root->right,targ,sum);
+ }
+
+    bool hasPathSum(TreeNode* root, int targetSum) {
+
+  return rec(root,targetSum,0);
         
     }
 };
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
