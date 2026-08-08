a0caa7a56e067d895dce3abd71e8ba2f4e8fbd21
PRASANNA S <psp444333@gmail.com>
2026-08-08 12:35:58 +0530

    3 ms (56.25%) 14 MB (68.07%) 0719-find-k-th-smallest-pair-distance Hard Array

---
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int isposs(vector<int>& nums,int diff,int k)
+    {
+        int l=0,pair=0;
+
+        for(int r=1;r<nums.size();r++)
+        {
+            while((nums[r]-nums[l])>diff)
+            {
+                l++;
+            }
+            pair+=(r-l);
+        }
+        
+
+        return pair;
+    }
+    int smallestDistancePair(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int l=0,r=nums[nums.size()-1]-nums[0],ans=r;
+
+
+        while(l<r)
+        {
+            int mid=l+(r-l)/2;
+            if(isposs(nums,mid,k)<k)
+            {
+                ans=mid;
+                 l=mid+1;
+            }
+            else
+            {
+                r=mid;
+            }
+
+        }
+        return l;
+    }
+};
