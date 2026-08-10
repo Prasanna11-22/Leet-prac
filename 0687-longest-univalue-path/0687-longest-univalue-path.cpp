e7a0d5b0230d7941568bb4c6c6ccd10a6b92b10b
PRASANNA S <psp444333@gmail.com>
2026-08-10 11:57:10 +0530

    0 ms (100%) 72.2 MB (81.08%) 0687-longest-univalue-path Medium Tree

--- 0687-longest-univalue-path/0687-longest-univalue-path.cpp
+++ 0687-longest-univalue-path/0687-longest-univalue-path.cpp
@@ -6,38 +6,33 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-int ans=0;
-int postord(TreeNode* root)
-{
-    if(!root) return 0;
-    int left=postord(root->left);
-    int right=postord(root->right);
+    int ans = 0;
+    int postord(TreeNode* root) {
+        if (!root)
+            return 0;
+        int left = postord(root->left);
+        int right = postord(root->right);
 
-    int l=0,r=0;
-   
-   if((root->left)&&root->val==root->left->val)
-   {
-         l=left+1;
-   }
-    if((root->right)&&root->val==root->right->val)   
-    {
-        r=right+1;
+        int l = 0, r = 0;
+
+        if ((root->left) && root->val == root->left->val) {
+            l = left + 1;
+        }
+        if ((root->right) && root->val == root->right->val) {
+            r = right + 1;
+        }
+        ans = max(ans, (l + r));
+        return max(l, r);
     }
-    ans=max(ans,(l+r));
-
-    cout<<ans<<endl;
-
-    return max(l,r);
-}
     int longestUnivaluePath(TreeNode* root) {
 
         postord(root);
-        return ans; 
-
+        return ans;
     }
 };
