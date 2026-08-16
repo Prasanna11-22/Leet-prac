e16574e2109d42bace0885b3427b01de2064381d
PRASANNA S <psp444333@gmail.com>
2026-08-16 22:17:15 +0530

    68 ms (32.27%) 43.8 MB (79.93%) 0236-lowest-common-ancestor-of-a-binary-tree Medium Tree

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,31 +9,21 @@
  */
 class Solution {
 public:
-TreeNode* lca(TreeNode* root,TreeNode* p,TreeNode* q)
-{
-    if(root==nullptr||root==p||root==q)
-    {
-        return root;
-    }
-
-    TreeNode* l=lca(root->left,p,q);
-    TreeNode* r=lca(root->right,p,q);
-    
-
-    if(l==nullptr)
-    {
-       return r;
-    }
-    else if(r==nullptr)
-
-    {
-        return l;
-    }
-
-    return root;
-}
-
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return lca(root,p,q);
+        if(root==nullptr||root==p||root==q)
+        {
+            return root;
+        }
+
+        TreeNode* l=lowestCommonAncestor(root->left,p,q); 
+        TreeNode* r=lowestCommonAncestor(root->right,p,q); 
+
+        if(l&&r)
+        {
+            return root;
+        }
+    
+        return (l)?l:r;
+        
     }
 };
