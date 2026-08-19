c8e2555d9a1f6bfd74332320b0d1455a980d19fc
PRASANNA S <psp444333@gmail.com>
2026-08-19 14:19:57 +0530

    422 ms (29.41%) 329.2 MB (22.9%) 1971-find-if-path-exists-in-graph Easy Depth-First Search

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+vector<vector<int>> adj;
+vector<int> vis;
+void dfs(int node)
+{
+    vis[node]=1;
+    for(auto nei : adj[node])
+    {
+        if(!vis[nei])
+        {
+            dfs(nei);
+        }
+    }
+}
+
+bool validPath(int n, vector<vector<int>>& edges, int src, int dest) {
+    adj.resize(n+1);
+    vis.resize(n+1,0);
+
+    for(auto k : edges)
+    {
+        int u=k[0],v=k[1];
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    dfs(src);
+    return (vis[dest]==1);
+    }
+};
