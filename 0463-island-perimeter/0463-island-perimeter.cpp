d6e6d13aa089fcd98c44b9c1f2da99cbb639b7ee
PRASANNA S <psp444333@gmail.com>
2026-08-22 14:58:08 +0530

    12 ms (25.05%) 106.1 MB (13.49%) 0463-island-perimeter Easy Array

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,49 +1,40 @@
 class Solution {
 public:
-int r,c;
+int r,c,res=0;
 vector<vector<int>> vis;
 
-int dfs(int i,int j,vector<vector<int>>& grid)
+void dfs(int i,int j,vector<vector<int>>& grid)
 {
-    if(i<0||j<0||i>=r||j>=c)
+    if(i<0||j<0||i>=r||j>=c||grid[i][j]==0)
     {
-        return 1;
+        res++;
+        return;
     }
     
-    if(grid[i][j]==1&&vis[i][j]) return 0;
-    else if(grid[i][j]==0)
-    {
-    if(!vis[i][j])
-    {
-        vis[i][j]=1;
-    }
-    return 1;
-    }
+    if(grid[i][j]==-1) return;
     
-    vis[i][j]=1;
-    return dfs(i-1,j,grid)+dfs(i+1,j,grid)+dfs(i,j-1,grid)+dfs(i,j+1,grid); 
+    grid[i][j]=-1;
+
+    dfs(i-1,j,grid);
+    dfs(i+1,j,grid);
+    dfs(i,j-1,grid);
+    dfs(i,j+1,grid); 
 }
 
     int islandPerimeter(vector<vector<int>>& grid) {
-        int res=0;
-        bool chk=true;
         r=grid.size(),c=grid[0].size();
         vis.resize(r+1,vector<int>(c+1,0));
         for(int i=0;i<r;i++)
         {
             for(int j=0;j<c;j++)
             {
-                if(chk)
-                {
                 if(grid[i][j]==1)
                 {
-                  res=dfs(i,j,grid);
-                  chk=false; 
-                }
+                  dfs(i,j,grid);
+                  return res;
                 }
             }
         }
-
-        return res;
+        return -1;
     }
 };
