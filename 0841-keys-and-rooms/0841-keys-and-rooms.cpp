42d350720e2f32ee72ae77fda7da7db803550168
PRASANNA S <psp444333@gmail.com>
2026-08-22 15:10:41 +0530

    0 ms (100%) 14.2 MB (97.2%) 0841-keys-and-rooms Medium Depth-First Search

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-vector<int> vis;
-
-void dfs(int node,vector<vector<int>>&rooms)
-{
-    vis[node]=1;
-    for(int i : rooms[node])
-    {
-        if(!vis[i])
-        {
-            dfs(i,rooms);
-        }
-    }
-}
-
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n=rooms.size();
-        vis.resize(n+1,0);
+        vector<bool> vis(n+1,false);
+        queue<int> q;
+        q.push(0);
+        vis[0]=true;
 
-        dfs(0,rooms);
-        for(int i=0;i<n;i++)
+        while(!q.empty())
         {
-            if(!vis[i])
+            int node=q.front();
+            q.pop();
+
+            for(int nei : rooms[node])
             {
-                return false;
+                if(!vis[nei])
+                {
+                    vis[nei]=true;
+                    q.push(nei);
+                }
             }
         }
+
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i]) return false;
+        }
         return true;
+
+        
     }
 };
