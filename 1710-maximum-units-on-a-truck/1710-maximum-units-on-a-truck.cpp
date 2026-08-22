883688a9b64076cae091c645d2f8bc7839b336d2
PRASANNA S <psp444333@gmail.com>
2026-08-22 12:37:44 +0530

    5 ms (39.97%) 20.9 MB (18.65%) 1710-maximum-units-on-a-truck Easy Array

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-static bool cmp(vector<int>&a,vector<int>&b)
-{
-    return a[1]>b[1];
-}
     int maximumUnits(vector<vector<int>>& boxes, int truckSize) {
-        sort(boxes.begin(),boxes.end(),cmp);
+        sort(boxes.begin(),boxes.end(),[](vector<int>&a,vector<int>&b){
+            return a[1]>b[1]; 
+        });
         int ans=0;
 
         for(auto k : boxes)
