58763c8281b91da7e2a0c965b1edfd0f23b58126
PRASANNA S <psp444333@gmail.com>
2026-08-25 14:08:08 +0530

    3 ms (21.71%) 9.3 MB (32.62%) 2259-remove-digit-from-number-to-maximize-result Easy String

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     string removeDigit(string num, char digit) {
-        string res="";
         string maxi="";
 
         for(int i=0;i<num.size();i++)
         {
             if(num[i]==digit) 
             {
-                string str1=num.substr(0,i);
-                string str2=num.substr(i+1,num.size());
-                str1=str1+str2;
-                maxi=max((str1),maxi);
+                string str1=num.substr(0,i)+num.substr(i+1,num.size());
+                maxi=max(str1,maxi);
             }
         }
         return maxi;
