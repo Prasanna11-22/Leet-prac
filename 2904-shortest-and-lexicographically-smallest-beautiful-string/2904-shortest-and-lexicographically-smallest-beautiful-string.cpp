1b99cd96944e1e654247dcee7e1184e15590ee40
PRASANNA S <psp444333@gmail.com>
2026-08-26 13:14:22 +0530

    3 ms (26.7%) 8.9 MB (37.53%) 2904-shortest-and-lexicographically-smallest-beautiful-string Medium String

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        int l=0,cnt=0,mini=INT_MAX,minlen=INT_MAX;
-        string ans="",minis=s;
-        for(int r=0;r<s.size();r++)
+        int l = 0, cnt = 0, mini = INT_MAX, minlen = INT_MAX;
+        string ans = "", minis = s;
+        for (int r = 0; r < s.size(); r++) 
         {
-            if(s[r]=='1') cnt++;
+            if (s[r] == '1')
+                cnt++;
 
-            while(cnt>k)
+            while (cnt > k) 
             {
-               if(s[l]=='1') cnt--;   
+                if (s[l] == '1')
+                    cnt--;
                 l++;
             }
-                if(cnt==k)
+            if (cnt == k) 
+            {
+                while (s[l] == '0')
+                    l++;
+                if (minlen >= r - l + 1) 
                 {
-                while(s[l]=='0') l++;
-                if(minlen>=r-l+1)
-                {
-                    if(minlen==r-l+1)
+                    if (minlen == r - l + 1) 
                     {
-                     ans=min(ans,s.substr(l,r-l+1));
-                    }
-                    else
+                        ans = min(ans, s.substr(l, r - l + 1));
+                    } 
+                    else 
                     {
-                       ans=s.substr(l,r-l+1); 
+                        ans = s.substr(l, r - l + 1);
                     }
-                    // cout<<ans<<endl;
-                    minlen=r-l+1;
+                    cout << ans << endl;
+                    minlen = r - l + 1;
                 }
-                }
+            }
         }
         return ans;
     }
