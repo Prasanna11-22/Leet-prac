c79feb450eab6515d7a4fd5105f9296d0554657a
PRASANNA S <psp444333@gmail.com>
2026-08-28 10:22:19 +0530

    856 ms (5.04%) 30 MB (25.68%) 0204-count-primes Medium Array

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        int isPrime[n];
+        memset(isPrime,1,sizeof(isPrime));
+        isPrime[0] = isPrime[1] = 0;
+        int count = n-2;
+
+        for (int i = 2; i * i < n; i++) 
+        {
+            if(isPrime[i])
+            {
+
+            for (int j = i * i; j < n; j += i) 
+                {
+                    if (isPrime[j]) 
+                    {   
+                    isPrime[j] = 0;
+                    count--;
+                    }
+                }
+            }
+        }
+
+
+        return count;
+    }
+};
