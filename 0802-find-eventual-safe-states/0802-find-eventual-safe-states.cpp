cff2a1484e0103cf337fd49094db7b67c64d68e1
PRASANNA S <psp444333@gmail.com>
2026-08-28 12:22:55 +0530

    2 ms (95.48%) 51.4 MB (96.87%) 0802-find-eventual-safe-states Medium Depth-First Search

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+vector<bool>vis;
+vector<bool>inpath;
+vector<bool>chk;
+
+bool dfs(int n ,vector<vector<int>>& graph)
+{
+    vis[n]=true;
+    inpath[n]=true;
+
+    for(int nei : graph[n])
+    {
+        if(!vis[nei])
+        {
+            if(dfs(nei,graph))
+            {
+                return true;
+            }
+        }
+        else if(inpath[nei])
+        {
+            return true;
+
+        }
+    }
+
+    inpath[n]=false; 
+    chk[n]=true;
+    return false;
+}
+
+
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int V=graph.size();
+        vis.assign(V,false);
+        inpath.assign(V,false);
+        chk.assign(V,false);
+        vector<int> res;
+
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i])
+            {
+                dfs(i,graph);
+            }
+        }
+
+        for(int i=0;i<V;i++)
+        { 
+            if(chk[i]==true)
+            {
+                res.push_back(i);
+            }
+        }
+        
+        return res;
+    }
+};
