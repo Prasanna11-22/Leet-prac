decf9bbb8d5fb9a43c0732bbee943aa605ec299c
PRASANNA S <psp444333@gmail.com>
2026-09-01 10:04:57 +0530

    4 ms (52.58%) 15 MB (27.23%) 0001-two-sum Easy Array

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> mp;
-        vector<int> res;
-        
+
         for(int i=0;i<nums.size();i++)
         {
         int chk=target-nums[i];
         if(mp.count(chk))
         {
-            res.push_back(mp[chk]);
-            res.push_back(i);
+            return{mp[chk],i};
         }
         else
         {
             mp[nums[i]]=i;
         }
         }
-    return res;
+    return {};
     }
 };
