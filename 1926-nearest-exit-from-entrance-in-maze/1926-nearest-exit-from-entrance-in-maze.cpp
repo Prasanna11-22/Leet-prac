48bd26820cb23c33cee90ab0ca1890c8a207d1b8
PRASANNA S <psp444333@gmail.com>
2026-09-01 15:52:58 +0530

    7 ms (60.76%) 36.8 MB (43.1%) 1926-nearest-exit-from-entrance-in-maze Medium Array

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+class tp {
+public:
+int i,j,cnt;
+};
+
+vector<int> dx={1,-1,0,0};
+vector<int> dy={0,0,1,-1};
+    int nearestExit(vector<vector<char>>& maze, vector<int>& ent) {
+        int r=maze.size(),c=maze[0].size();
+        vector<vector<bool>> vis(r,vector<bool>(c,false));
+        queue<tp> q;
+        q.push({ent[0],ent[1]});
+        vis[ent[0]][ent[1]]=true;
+
+        while(!q.empty())
+        {
+            auto [x,y,cnt]=q.front();
+            q.pop();
+
+            for(int i=0;i<4;i++)
+            {
+                int nr=x+dx[i],nc=y+dy[i];
+                if(nr<0||nc<0||nr>=r||nc>=c) continue;
+                if((!vis[nr][nc])&&(maze[nr][nc]=='.'))
+                {
+                    vis[nr][nc]=true;
+                    q.push({nr,nc,cnt+1});
+
+                 if((nr==r-1||nc==c-1||nr==0||nc==0))
+                {
+                    return cnt+1;
+                 }
+                }
+            }
+        }
+        return -1;
+    }
+};
