caf92a95be80f12ded864c2b5a69552cd7fd957d
PRASANNA S <psp444333@gmail.com>
2026-09-03 10:21:17 +0530

    1 ms (22.17%) 25.9 MB (77.64%) 0042-trapping-rain-water Hard Array

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        int lmax=0,rmax=0;
+        int tot=0;
+        
+        int l=0,r=h.size()-1;
+
+        while(l<=r)
+        {
+            lmax=max(lmax,h[l]);
+            rmax=max(rmax,h[r]);
+
+            if(h[l]<=h[r])
+            {
+                tot+=lmax-h[l];
+                l++;
+            }
+            else if(h[r]<h[l])
+            {
+                tot+=rmax-h[r];
+                r--;
+            }
+        }
+        return tot;
+    }
+};
